697453eb2c583e2fbc8d56539fa7104563b1fab5
M7mdxzxa <129904682+M7mdxzxa@users.noreply.github.com>
2024-11-02 15:02:36 +0200

    Add files via upload

---
+++ api.js
@@ -0,0 +1,7 @@
+const fastify = require("fastify")(); // a new fastify app
+const { attachApi } = require("siki");
+
+(async()=> {
+    const app = await attachApi(fastify) // returns the fastifyApp after attaching certian routes.
+    await app.listen({ port: 8080 }, ()=> console.log("Server is up!")) // starting the server!
+})()
